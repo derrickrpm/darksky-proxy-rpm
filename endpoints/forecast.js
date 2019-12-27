c4dfcb8a8614a86df04db1f4f1f7555c0d3c2f56
derrickrpm <derrick@rpmstaffing.net>
2019-12-27 12:42:34 -0600

    Update forecast.js
    fix whitelist like api key

--- endpoints/forecast.js
+++ endpoints/forecast.js
@@ -2,7 +2,7 @@ const request = require("request-promise");
 
 const API_URL = `https://api.darksky.net/forecast/${process.env.API_KEY}`;
 
-let { CORS_WHITELIST } = process.env;
+let { CORS_WHITELIST } = process.env.CORS_WHITELIST;
 if (CORS_WHITELIST) {
   CORS_WHITELIST = CORS_WHITELIST.replace(/ /g, "").split(",");
 }
